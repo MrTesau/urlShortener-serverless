66232d75fa484e8577cc58488b32141f3afa8b31
Tayyib Esau <esamu161@gmail.com>
2020-12-27 17:10:32 +1300

    Update [link].js

--- api/shorturl/[link].js
+++ api/shorturl/[link].js
@@ -3,99 +3,6 @@ var URL = require("url").URL;
 var mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-module.exports = async (req, res) => {
-  if (req.method === "GET") {
-    const reg1 = /^https:\/\//;
-    const reg2 = /^http:\/\//;
-    //const reg3 = /\/$/;
-    const db = await connectToDb();
-    const collection = await db.collection("urldatas");
-    let short = parseInt(req.query.link);
-    await collection
-      .find({ urlShort: short })
-      .toArray()
-      .then((data) => {
-        //console.log(data[0]);
-        if (!data[0].urlLong) {
-          res.json({ error: "No shortened URL found" });
-        } else if (reg1.test(data[0].urlLong) || reg2.test(data[0].urlLong)) {
-          res.redirect(data[0].urlLong);
-        } else {
-          res.redirect("https://" + data[0].urlLong);
-        }
-      });
-    // Even though :new is a query setup as [link]
-    // You can still acess it via api/shorturl/new as long as its a POST
-    // otherwise it will attempt to GET url
-  } else if (req.method === "POST") {
-    const stringIsAValidUrl = (str) => {
-      try {
-        new URL(str);
-        return true;
-      } catch (err) {
-        return false;
-      }
-    };
-    const db = await connectToDb();
-    const collection = await db.collection("urldatas");
-    const urlDataSchema = new Schema({
-      urlLong: { type: String, unique: true },
-      urlShort: Number,
-    });
-    const UrlData = mongoose.model("UrlData", urlDataSchema);
-    var postedURL = req.body.url;
-    //console.log(postedURL);
-    // replace problem characters
-
-    if (!stringIsAValidUrl(postedURL)) {
-      res.json({
-        error: "invalid url",
-      });
-    } else if (
-      new URL(postedURL).protocol != "http:" &&
-      new URL(postedURL).protocol != "https:"
-    ) {
-      res.json({
-        error: "invalid url",
-      });
-    } else {
-      await collection
-        .find({})
-        .toArray()
-        .then(async (data) => {
-          let docNum = data.length;
-          //console.log(docNum);
-          var newEntry = new UrlData({
-            urlLong: postedURL,
-            urlShort: docNum,
-          });
-          let checkUrl = data.filter((url) => url.urlLong === postedURL);
-          if (checkUrl.length) {
-            //console.log(checkUrl[0]);
-            res.json({
-              original_url: checkUrl[0].urlLong,
-              short_url: checkUrl[0].urlShort,
-            });
-          } else {
-            await collection.insertOne(newEntry).then((data, err) => {
-              //console.log(data);
-              if (err) return console.error(err);
-              res.json({
-                original_url: data.ops[0].urlLong,
-                short_url: data.ops[0].urlShort,
-              });
-            });
-          }
-        });
-    }
-  }
-};
-/*
-import { connectToDb } from "../../lib/database.js";
-var URL = require("url").URL;
-var mongoose = require("mongoose");
-const { Schema } = mongoose;
-
 // To allow cors the handler function for route must be wrapped in a cors function
 const allowCors = (fn) => async (req, res) => {
   res.setHeader("Access-Control-Allow-Credentials", true);
@@ -205,4 +112,3 @@ const handler = async (req, res) => {
 };
 
 module.exports = allowCors(handler);
-*/
