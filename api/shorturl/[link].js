91fa3f07bc9ed2060a3d14b89609edf3f28fbb83
Tayyib Esau <esamu161@gmail.com>
2020-12-27 16:51:29 +1300

    Update [link].js

--- api/shorturl/[link].js
+++ api/shorturl/[link].js
@@ -7,7 +7,12 @@ const { Schema } = mongoose;
 const allowCors = (fn) => async (req, res) => {
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
   res.setHeader(
     "Access-Control-Allow-Headers",
     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
